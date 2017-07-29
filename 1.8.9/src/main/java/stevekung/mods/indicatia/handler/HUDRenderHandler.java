3615469cb0138931711006d1f94fcdafa232d4cc
SteveKunG <mccommander_minecraft@hotmail.com>
2017-07-29 14:26:49 +0700

    1.8.9: Added More Donator text options

--- 1.8.9/src/main/java/stevekung/mods/indicatia/handler/HUDRenderHandler.java
+++ 1.8.9/src/main/java/stevekung/mods/indicatia/handler/HUDRenderHandler.java
@@ -135,11 +135,13 @@ public class HUDRenderHandler
                 {
                     if (!HUDRenderHandler.topDonator.isEmpty())
                     {
-                        leftInfo.add(HUDRenderHandler.topDonator);
+                        String text = ExtendedConfig.TOP_DONATOR_TEXT.isEmpty() ? "" : ExtendedConfig.TOP_DONATOR_TEXT + EnumChatFormatting.RESET + " ";
+                        leftInfo.add(text + HUDRenderHandler.topDonator);
                     }
                     if (!HUDRenderHandler.recentDonator.isEmpty())
                     {
-                        leftInfo.add(HUDRenderHandler.recentDonator);
+                        String text = ExtendedConfig.RECENT_DONATOR_TEXT.isEmpty() ? "" : ExtendedConfig.RECENT_DONATOR_TEXT + EnumChatFormatting.RESET + " ";
+                        leftInfo.add(text + HUDRenderHandler.recentDonator);
                     }
                 }
                 // server tps
@@ -186,11 +188,13 @@ public class HUDRenderHandler
                 {
                     if (!HUDRenderHandler.topDonator.isEmpty())
                     {
-                        rightInfo.add(HUDRenderHandler.topDonator);
+                        String text = ExtendedConfig.TOP_DONATOR_TEXT.isEmpty() ? "" : ExtendedConfig.TOP_DONATOR_TEXT + EnumChatFormatting.RESET + " ";
+                        rightInfo.add(text + HUDRenderHandler.topDonator);
                     }
                     if (!HUDRenderHandler.recentDonator.isEmpty())
                     {
-                        rightInfo.add(HUDRenderHandler.recentDonator);
+                        String text = ExtendedConfig.RECENT_DONATOR_TEXT.isEmpty() ? "" : ExtendedConfig.RECENT_DONATOR_TEXT + EnumChatFormatting.RESET + " ";
+                        rightInfo.add(text + HUDRenderHandler.recentDonator);
                     }
                 }
 
