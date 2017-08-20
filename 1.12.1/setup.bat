cbc7f3e466088485ef0129b8261f27fc0196c6d4
SteveKunG <mccommander_minecraft@hotmail.com>
2017-08-20 20:03:41 +0700

    -.-

--- 1.12.1/setup.bat
+++ 1.12.1/setup.bat
@@ -1,2 +1,2 @@
-set GRADLE_USER_HOME=M:\Minecraft Modding Workspace\Indicatia\1.12.1\.gradle_data
+set GRADLE_USER_HOME=M:\Modding\Indicatia\1.12.1\.gradle_data
 gradlew setupDecompWorkspace eclipse
