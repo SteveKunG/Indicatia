5bb3beec7da5f35aa829831753b1074ab51a2df8
SteveKunG <mccommander_minecraft@hotmail.com>
2017-08-25 16:30:16 +0700

    1.10.2: Improve Keystroke/Custom CPS rendering

--- 1.10.2/src/main/java/stevekung/mods/indicatia/handler/CommonHandler.java
+++ 1.10.2/src/main/java/stevekung/mods/indicatia/handler/CommonHandler.java
@@ -43,8 +43,6 @@ import stevekung.mods.indicatia.config.ConfigManager;
 import stevekung.mods.indicatia.config.ExtendedConfig;
 import stevekung.mods.indicatia.core.IndicatiaMod;
 import stevekung.mods.indicatia.gui.*;
-import stevekung.mods.indicatia.renderer.HUDInfo;
-import stevekung.mods.indicatia.renderer.KeystrokeRenderer;
 import stevekung.mods.indicatia.renderer.LayerAllArmor;
 import stevekung.mods.indicatia.renderer.LayerCustomCape;
 import stevekung.mods.indicatia.util.*;
@@ -260,25 +258,6 @@ public class CommonHandler
                 }
             }
             InfoUtil.INSTANCE.processMouseOverEntity(this.mc);
-        }
-        if (event.phase == TickEvent.Phase.END)
-        {
-            if (!this.mc.gameSettings.hideGUI && !this.mc.gameSettings.showDebugInfo)
-            {
-                if (ConfigManager.enableKeystroke)
-                {
-                    if (this.mc.currentScreen == null || this.mc.currentScreen instanceof GuiChat || this.mc.currentScreen instanceof GuiRenderStatusSettings || this.mc.currentScreen instanceof GuiKeystrokeColorSettings)
-                    {
-                        KeystrokeRenderer.init(this.mc);
-                    }
-                }
-                if (ConfigManager.enableRenderInfo && ExtendedConfig.CPS_POSITION.equalsIgnoreCase("custom") && (this.mc.currentScreen == null || this.mc.currentScreen instanceof GuiChat || this.mc.currentScreen instanceof GuiRenderStatusSettings || this.mc.currentScreen instanceof GuiKeystrokeColorSettings))
-                {
-                    String space = ConfigManager.enableRCPS ? " " : "";
-                    RenderUtil.drawRect(ExtendedConfig.CPS_X_OFFSET, ExtendedConfig.CPS_Y_OFFSET, ExtendedConfig.CPS_X_OFFSET + this.mc.fontRendererObj.getStringWidth(HUDInfo.getCPS() + space + HUDInfo.getRCPS()) + 4, ExtendedConfig.CPS_Y_OFFSET + 11, 16777216, ExtendedConfig.CPS_OPACITY);
-                    this.mc.fontRendererObj.drawString(HUDInfo.getCPS() + space + HUDInfo.getRCPS(), ExtendedConfig.CPS_X_OFFSET + 2, ExtendedConfig.CPS_Y_OFFSET + 2, 16777215, true);
-                }
-            }
         }
     }
 
