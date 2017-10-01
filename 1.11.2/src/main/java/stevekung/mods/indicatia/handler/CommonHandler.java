ead027d6caef8ecc568afae7692e3d9e53d85ad6
SteveKunG <mccommander_minecraft@hotmail.com>
2017-10-01 14:25:36 +0700

    1.11.2: Fixed Cape rendering. Improve MC-75279

--- 1.11.2/src/main/java/stevekung/mods/indicatia/handler/CommonHandler.java
+++ 1.11.2/src/main/java/stevekung/mods/indicatia/handler/CommonHandler.java
@@ -26,6 +26,7 @@ import net.minecraft.client.multiplayer.ServerAddress;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.renderer.entity.*;
 import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+import net.minecraft.client.renderer.entity.layers.LayerCape;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.AbstractSkeleton;
@@ -62,6 +63,7 @@ import stevekung.mods.indicatia.config.ExtendedConfig;
 import stevekung.mods.indicatia.core.IndicatiaMod;
 import stevekung.mods.indicatia.gui.*;
 import stevekung.mods.indicatia.renderer.LayerAllArmor;
+import stevekung.mods.indicatia.renderer.LayerCapeNew;
 import stevekung.mods.indicatia.renderer.LayerCustomCape;
 import stevekung.mods.indicatia.util.*;
 
@@ -219,6 +221,8 @@ public class CommonHandler
             RenderPlayer renderSlim = manager.getSkinMap().get("slim");
             CommonHandler.replaceArmorLayer(layerLists, new LayerAllArmor<>(renderDefault, entity), renderer, entity);
             CommonHandler.replaceArmorLayer(layerLists, new LayerAllArmor<>(renderSlim, entity), renderer, entity);
+            CommonHandler.replaceCapeLayer(layerLists, new LayerCapeNew(renderDefault));
+            CommonHandler.replaceCapeLayer(layerLists, new LayerCapeNew(renderSlim));
         }
         else if (entity instanceof EntityZombieVillager)
         {
@@ -728,6 +732,25 @@ public class CommonHandler
                     });
                 }
             }
+        }
+    }
+
+    private static void replaceCapeLayer(List<LayerRenderer> layerLists, LayerRenderer newLayer)
+    {
+        int capeLayerIndex = -1;
+
+        for (int i = 0; i < layerLists.size(); i++)
+        {
+            LayerRenderer layer = layerLists.get(i);
+
+            if (layer instanceof LayerCape)
+            {
+                capeLayerIndex = i;
+            }
+        }
+        if (capeLayerIndex >= 0)
+        {
+            layerLists.set(capeLayerIndex, newLayer);
         }
     }
 
