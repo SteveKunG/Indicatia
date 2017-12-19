b8f29c6f9c77adff13b6071ef95e111bb36df4f8
SteveKunG <mccommander_minecraft@hotmail.com>
2017-12-19 13:30:10 +0700

    Update stuff

--- 1.12.2/setup.bat
+++
@@ -1,2 +0,0 @@
-set GRADLE_USER_HOME=M:\Modding\Indicatia\1.12.2\.gradle_data
-gradlew setupDecompWorkspace eclipse
--- 1.8.9/upload.bat
+++
@@ -1,2 +0,0 @@
-set GRADLE_USER_HOME=M:\Modding\Indicatia\1.8.9\.gradle_data
-gradlew curseforge
