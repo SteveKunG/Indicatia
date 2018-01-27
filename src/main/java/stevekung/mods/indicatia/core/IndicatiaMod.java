eb0a9c61464087f3b9e22f99747b3e69a1f32cb0
SteveKunG <mccommander_minecraft@hotmail.com>
2018-01-27 15:10:47 +0700

    Added Jar Signing

--- src/main/java/stevekung/mods/indicatia/core/IndicatiaMod.java
+++ src/main/java/stevekung/mods/indicatia/core/IndicatiaMod.java
@@ -18,6 +18,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.ModMetadata;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -33,7 +34,7 @@ import stevekung.mods.indicatia.renderer.RenderFishNew;
 import stevekung.mods.indicatia.renderer.TileEntitySkullRendererNew;
 import stevekung.mods.indicatia.util.*;
 
-@Mod(modid = IndicatiaMod.MOD_ID, name = IndicatiaMod.NAME, version = IndicatiaMod.VERSION, dependencies = IndicatiaMod.FORGE_VERSION, clientSideOnly = true, guiFactory = IndicatiaMod.GUI_FACTORY)
+@Mod(modid = IndicatiaMod.MOD_ID, name = IndicatiaMod.NAME, version = IndicatiaMod.VERSION, dependencies = IndicatiaMod.FORGE_VERSION, clientSideOnly = true, guiFactory = IndicatiaMod.GUI_FACTORY, certificateFingerprint = IndicatiaMod.CERTIFICATE)
 public class IndicatiaMod
 {
     public static final String NAME = "Indicatia";
@@ -46,6 +47,7 @@ public class IndicatiaMod
     public static final String GUI_FACTORY = "stevekung.mods.indicatia.config.ConfigGuiFactory";
     public static final String FORGE_VERSION = "after:Forge@[11.15.1.2318,);";
     public static final String URL = "https://minecraft.curseforge.com/projects/indicatia";
+    public static final String CERTIFICATE = "@FINGERPRINT@";
     private static boolean DEOBFUSCATED;
     public static Minecraft MC;
     public static boolean CHECK_NO_CONNECTION;
@@ -128,6 +130,19 @@ public class IndicatiaMod
         ((IReloadableResourceManager)IndicatiaMod.MC.getResourceManager()).registerReloadListener(IndicatiaMod.coloredFontRenderer);
         TileEntityRendererDispatcher.instance.mapSpecialRenderers.entrySet().removeIf(entry -> entry.getKey().equals(TileEntitySkull.class));
         TileEntityRendererDispatcher.instance.mapSpecialRenderers.put(TileEntitySkull.class, new TileEntitySkullRendererNew());
+    }
+
+    @EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event)
+    {
+        if (IndicatiaMod.isObfuscatedEnvironment())
+        {
+            ModLogger.info("Development environment detected! Ignore certificate check.");
+        }
+        else
+        {
+            throw new RuntimeException("Invalid fingerprint detected! This version will NOT be supported by the author!");
+        }
     }
 
     public static boolean isObfuscatedEnvironment()
