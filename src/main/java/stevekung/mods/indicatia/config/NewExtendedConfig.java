dfce84ea5eb96d181f3738d6c12f73f1a4bd4d2f
SteveKunG <mccommander_minecraft@hotmail.com>
2018-03-18 18:22:07 +0700

    Correct move file

--- src/main/java/stevekung/mods/indicatia/config/NewExtendedConfig.java
+++ src/main/java/stevekung/mods/indicatia/config/NewExtendedConfig.java
@@ -1,38 +1,43 @@
 package stevekung.mods.indicatia.config;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import stevekung.mods.indicatia.core.IndicatiaMod;
 
 public class NewExtendedConfig
 {
     private static final File OLD_FILE = new File(IndicatiaMod.MC.mcDataDir, "indicatia_data.dat");
-    private static final File FILE = new File(IndicatiaMod.MC.mcDataDir, "indicatia_profile/default_profile.dat");
-    
-    public static void moveOldConfig() throws IOException
+    private static final File DEST = new File(IndicatiaMod.MC.mcDataDir, "indicatia_profile");
+
+    public static void moveOldConfig()
     {
-       if (OLD_FILE.exists())
-       {
-           InputStream input = null;
-           OutputStream output = null;
-           
-           try
-           {
-               input = new FileInputStream(OLD_FILE);
-               output = new FileOutputStream(FILE);
-               byte[] buffer = new byte[1024];
-               int length;
-               
-               while ((length = input.read(buffer)) > 0)
-               {
-                  output.write(buffer, 0, length); 
-               }
-           }
-           finally
-           {
-               input.close();
-               output.close();
-           }
-       }
+        if (!DEST.exists())
+        {
+            if (!DEST.mkdirs())
+            {
+                return;
+            }
+        }
+
+        if (OLD_FILE.exists())
+        {
+            File destPath = new File(DEST, OLD_FILE.getName());
+
+            if (destPath.exists())
+            {
+                OLD_FILE.delete();
+                return;
+            }
+            try
+            {
+                Files.move(OLD_FILE.toPath(), destPath.toPath());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
