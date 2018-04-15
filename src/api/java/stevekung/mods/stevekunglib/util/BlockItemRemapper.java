5d21bac4a26e778563cd9310b8554d17073ed0bb
SteveKunG <mccommander_minecraft@hotmail.com>
2018-04-16 00:37:50 +0700

    Java 8 Cleanup

--- src/api/java/stevekung/mods/stevekunglib/util/BlockItemRemapper.java
+++ src/api/java/stevekung/mods/stevekunglib/util/BlockItemRemapper.java
@@ -8,37 +8,37 @@ public class BlockItemRemapper
 {
     public static void remapBlock(RegistryEvent.MissingMappings<Block> event, String modid, String oldName, Block block)
     {
-        for (RegistryEvent.MissingMappings.Mapping<Block> mappings : event.getMappings())
+        event.getMappings().forEach(mappings ->
         {
             if (mappings.key.getResourceDomain().equals(modid) && mappings.key.getResourcePath().equals(oldName))
             {
                 mappings.remap(block);
                 ModLogger.info("Remapping Block Complete (From {} to {})", mappings.key, block.getRegistryName());
             }
-        }
+        });
     }
 
     public static void remapItem(RegistryEvent.MissingMappings<Item> event, String modid, String oldName, Block block)
     {
-        for (RegistryEvent.MissingMappings.Mapping<Item> mappings : event.getMappings())
+        event.getMappings().forEach(mappings ->
         {
             if (mappings.key.getResourceDomain().equals(modid) && mappings.key.getResourcePath().equals(oldName))
             {
                 mappings.remap(Item.getItemFromBlock(block));
                 ModLogger.info("Remapping Block Complete (From {} to {})", mappings.key, block.getRegistryName());
             }
-        }
+        });
     }
 
     public static void remapItem(RegistryEvent.MissingMappings<Item> event, String modid, String oldName, Item item)
     {
-        for (RegistryEvent.MissingMappings.Mapping<Item> mappings : event.getMappings())
+        event.getMappings().forEach(mappings ->
         {
             if (mappings.key.getResourceDomain().equals(modid) && mappings.key.getResourcePath().equals(oldName))
             {
                 mappings.remap(item);
                 ModLogger.info("Remapping Block Complete (From {} to {})", mappings.key, item.getRegistryName());
             }
-        }
+        });
     }
 }
