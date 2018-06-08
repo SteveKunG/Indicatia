a73f6e39cb680a0bdb9f36234abc2a9266f60201
SteveKunG <mccommander_minecraft@hotmail.com>
2018-06-08 22:59:54 +0700

    Fix rare crash with dimension TPS

--- src/main/java/stevekung/mods/indicatia/event/HUDRenderEventHandler.java
+++ src/main/java/stevekung/mods/indicatia/event/HUDRenderEventHandler.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -209,11 +208,16 @@ public class HUDRenderEventHandler
 
                     if (ExtendedConfig.tpsAllDims)
                     {
-                        Arrays.stream(DimensionManager.getIDs()).forEach(dimensionIds ->
+                        for (Integer dimensionIds : DimensionManager.getIDs())
                         {
+                            if (dimensionIds == null)
+                            {
+                                ModLogger.error("Got null Dimension ID {}! Skipped TPS from dimension", dimensionIds);
+                                return;
+                            }
                             double dimensionTPS = HUDRenderEventHandler.mean(server.worldTickTimes.get(dimensionIds)) * 1.0E-6D;
                             leftInfo.add(ColorUtils.stringToRGB(ExtendedConfig.tpsColor).toColoredFont() + "Dimension " + server.getWorld(dimensionIds).provider.getDimensionType().getName() + " " + dimensionIds + ": " + ColorUtils.stringToRGB(ExtendedConfig.tpsValueColor).toColoredFont() + HUDRenderEventHandler.tpsFormat.format(dimensionTPS));
-                        });
+                        }
                     }
                     else
                     {
