07889b61dcfcd28d4059b0a7d9451ace257c66ae
SteveKunG <mccommander_minecraft@hotmail.com>
2018-11-19 22:33:54 +0700

    Catch JSON Syntax exception

--- src/main/java/stevekung/mods/indicatia/utils/ThreadMinigameData.java
+++ src/main/java/stevekung/mods/indicatia/utils/ThreadMinigameData.java
@@ -1,6 +1,7 @@
 package stevekung.mods.indicatia.utils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -8,10 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 
 import stevekung.mods.indicatia.minigames.MinigameCommand;
 import stevekung.mods.indicatia.minigames.MinigameData;
@@ -54,7 +52,7 @@ public class ThreadMinigameData extends Thread
                 MinigameData.addMinigameData(new MinigameData(name, minigameCommandList));
             }
         }
-        catch (Exception e)
+        catch (IOException | JsonSyntaxException e)
         {
             e.printStackTrace();
             LoggerIN.error("Could not getting minigames data from GitHub!");
