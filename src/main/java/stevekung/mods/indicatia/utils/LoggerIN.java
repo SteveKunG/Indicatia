268e291c777946a27672ef9e4a3dd6f5143bbe1d
SteveKunG <mccommander_minecraft@hotmail.com>
2019-01-15 09:27:29 +0700

    Added Marker to Logger

--- src/main/java/stevekung/mods/indicatia/utils/LoggerIN.java
+++ src/main/java/stevekung/mods/indicatia/utils/LoggerIN.java
@@ -2,38 +2,43 @@ package stevekung.mods.indicatia.utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 
 public class LoggerIN
 {
     private static final Logger LOG = LogManager.getLogger("Indicatia");
+    static final Marker INFO = MarkerManager.getMarker("INFO");
+    static final Marker ERROR = MarkerManager.getMarker("ERROR");
+    static final Marker WARNING = MarkerManager.getMarker("WARNING");
 
     public static void info(String message)
     {
-        LoggerIN.LOG.info(message);
+        LoggerIN.LOG.info(INFO, message);
     }
 
     public static void error(String message)
     {
-        LoggerIN.LOG.error(message);
+        LoggerIN.LOG.error(ERROR, message);
     }
 
     public static void warning(String message)
     {
-        LoggerIN.LOG.warn(message);
+        LoggerIN.LOG.warn(WARNING, message);
     }
 
     public static void info(String message, Object... obj)
     {
-        LoggerIN.LOG.info(message, obj);
+        LoggerIN.LOG.info(INFO, message, obj);
     }
 
     public static void error(String message, Object... obj)
     {
-        LoggerIN.LOG.error(message, obj);
+        LoggerIN.LOG.error(ERROR, message, obj);
     }
 
     public static void warning(String message, Object... obj)
     {
-        LoggerIN.LOG.warn(message, obj);
+        LoggerIN.LOG.warn(WARNING, message, obj);
     }
 }
