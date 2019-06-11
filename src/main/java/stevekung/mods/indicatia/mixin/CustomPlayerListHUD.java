08c3b03222ae965f939ad87995fce3dc9b216707
SteveKunG <mccommander_minecraft@hotmail.com>
2019-06-12 00:56:18 +0700

    Add @Final to PlayerListHud

--- src/main/java/stevekung/mods/indicatia/mixin/CustomPlayerListHUD.java
+++ src/main/java/stevekung/mods/indicatia/mixin/CustomPlayerListHUD.java
@@ -1,5 +1,6 @@
 package stevekung.mods.indicatia.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +14,7 @@ import stevekung.mods.indicatia.gui.overlay.PlayerListHudNew;
 @Mixin(InGameHud.class)
 public class CustomPlayerListHUD
 {
-    @Shadow
+    @Shadow @Final
     private PlayerListHud playerListHud;
 
     @Inject(at = @At("RETURN"), method = "(Lnet/minecraft/client/MinecraftClient;)V")
