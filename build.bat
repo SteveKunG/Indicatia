cfb2bb52f206505a621d03643067400fc2e74ac2
SteveKunG <mccommander_minecraft@hotmail.com>
2019-07-12 06:59:11 +0700

    Update

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-rd /s /q "M:\Modding\Indicatia\1.13-pre\build\classes"
-rd /s /q "M:\Modding\Indicatia\1.13-pre\build\dependency-cache"
-rd /s /q "M:\Modding\Indicatia\1.13-pre\build\libs"
-rd /s /q "M:\Modding\Indicatia\1.13-pre\build\resources"
-rd /s /q "M:\Modding\Indicatia\1.13-pre\build\sources"
+rd /s /q "M:\Modding\Indicatia\1.12.2\build\classes"
+rd /s /q "M:\Modding\Indicatia\1.12.2\build\dependency-cache"
+rd /s /q "M:\Modding\Indicatia\1.12.2\build\libs"
+rd /s /q "M:\Modding\Indicatia\1.12.2\build\resources"
+rd /s /q "M:\Modding\Indicatia\1.12.2\build\sources"
 set GRADLE_USER_HOME=C:\Users\SteveKunG\.gradle_1.12.2
 gradlew buildJar
