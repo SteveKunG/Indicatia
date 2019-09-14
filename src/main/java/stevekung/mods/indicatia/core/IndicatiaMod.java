de079f1d008f7d93ac2732eb94288e03a43e5a32
SteveKunG <mccommander_minecraft@hotmail.com>
2019-09-14 12:57:49 +0700

    Update Forge

--- src/main/java/stevekung/mods/indicatia/core/IndicatiaMod.java
+++ src/main/java/stevekung/mods/indicatia/core/IndicatiaMod.java
@@ -53,7 +53,7 @@ public class IndicatiaMod
     private static final int MINOR_VERSION = 3;
     private static final int BUILD_VERSION = 1;
     public static final String VERSION = IndicatiaMod.MAJOR_VERSION + "." + IndicatiaMod.MINOR_VERSION + "." + IndicatiaMod.BUILD_VERSION;
-    private static final String FORGE_VERSION = "after:forge@[14.23.5.2838,);";
+    private static final String FORGE_VERSION = "after:forge@[14.23.5.2847,);";
     protected static final String DEPENDENCIES = "required-after:stevekung's_lib@[1.1.3,); " + IndicatiaMod.FORGE_VERSION;
     private static final String URL = "https://minecraft.curseforge.com/projects/indicatia";
     protected static final String JSON_URL = "https://raw.githubusercontent.com/SteveKunG/VersionCheckLibrary/master/indicatia_version.json";
