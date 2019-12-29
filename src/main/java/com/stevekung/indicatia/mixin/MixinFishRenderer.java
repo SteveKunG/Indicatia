0e7e19649dfcb25909303aba800e6a4846c0115e
SteveKunG <mccommander_minecraft@hotmail.com>
2019-12-30 04:21:38 +0700

    Fish Bobber now rendered

--- src/main/java/com/stevekung/indicatia/mixin/MixinFishRenderer.java
+++ src/main/java/com/stevekung/indicatia/mixin/MixinFishRenderer.java
@@ -43,7 +43,7 @@ public abstract class MixinFishRenderer extends EntityRenderer<Entity>
             GlStateManager.scalef(0.5F, 0.5F, 0.5F);
             this.bindEntityTexture(entity);
             Tessellator tessellator = Tessellator.getInstance();
-            BufferBuilder vertexbuffer = tessellator.getBuffer();
+            BufferBuilder bufferbuilder = tessellator.getBuffer();
             GlStateManager.rotatef(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
             GlStateManager.rotatef((this.renderManager.options.thirdPersonView == 2 ? -1 : 1) * -this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
 
@@ -53,11 +53,11 @@ public abstract class MixinFishRenderer extends EntityRenderer<Entity>
                 GlStateManager.setupSolidRenderingTextureCombine(this.getTeamColor(entity));
             }
 
-            vertexbuffer.begin(GLConstants.QUADS, DefaultVertexFormats.POSITION_TEX_NORMAL);
-            vertexbuffer.pos(-0.5D, -0.5D, 0.0D).tex(0.0625D, 0.1875D).normal(0.0F, 1.0F, 0.0F).endVertex();
-            vertexbuffer.pos(0.5D, -0.5D, 0.0D).tex(0.125D, 0.1875D).normal(0.0F, 1.0F, 0.0F).endVertex();
-            vertexbuffer.pos(0.5D, 0.5D, 0.0D).tex(0.125D, 0.125D).normal(0.0F, 1.0F, 0.0F).endVertex();
-            vertexbuffer.pos(-0.5D, 0.5D, 0.0D).tex(0.0625D, 0.125D).normal(0.0F, 1.0F, 0.0F).endVertex();
+            bufferbuilder.begin(GLConstants.QUADS, DefaultVertexFormats.POSITION_TEX_NORMAL);
+            bufferbuilder.pos(-0.5D, -0.5D, 0.0D).tex(0.0D, 1.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
+            bufferbuilder.pos(0.5D, -0.5D, 0.0D).tex(1.0D, 1.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
+            bufferbuilder.pos(0.5D, 0.5D, 0.0D).tex(1.0D, 0.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
+            bufferbuilder.pos(-0.5D, 0.5D, 0.0D).tex(0.0D, 0.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
             tessellator.draw();
 
             if (this.renderOutlines)
@@ -68,20 +68,20 @@ public abstract class MixinFishRenderer extends EntityRenderer<Entity>
 
             GlStateManager.disableRescaleNormal();
             GlStateManager.popMatrix();
-            int k = player.getPrimaryHand() == HandSide.RIGHT ? 1 : -1;
+            int i = player.getPrimaryHand() == HandSide.RIGHT ? 1 : -1;
             ItemStack itemStack = player.getHeldItemMainhand();
 
             if (!(itemStack.getItem() instanceof FishingRodItem))
             {
-                k = -k;
+                i = -i;
             }
 
-            float f7 = player.getSwingProgress(partialTicks);
-            float f8 = MathHelper.sin(MathHelper.sqrt(f7) * (float)Math.PI);
-            float f9 = (player.prevRenderYawOffset + (player.renderYawOffset - player.prevRenderYawOffset) * partialTicks) * 0.017453292F;
-            double d0 = MathHelper.sin(f9);
-            double d1 = MathHelper.cos(f9);
-            double d2 = k * 0.35D;
+            float f3 = player.getSwingProgress(partialTicks);
+            float f4 = MathHelper.sin(MathHelper.sqrt(f3) * (float)Math.PI);
+            float f5 = MathHelper.lerp(partialTicks, player.prevRenderYawOffset, player.renderYawOffset) * ((float)Math.PI / 180F);
+            double d0 = MathHelper.sin(f5);
+            double d1 = MathHelper.cos(f5);
+            double d2 = i * 0.35D;
             double d4;
             double d5;
             double d6;
@@ -90,43 +90,44 @@ public abstract class MixinFishRenderer extends EntityRenderer<Entity>
 
             if ((this.renderManager.options == null || this.renderManager.options.thirdPersonView <= 0) && player == Minecraft.getInstance().player)
             {
-                double f10 = this.renderManager.options.fov;
-                f10 = f10 / 100.0F;
-                Vec3d vec3d = IndicatiaConfig.GENERAL.enableOldFishingRodRender.get() ? new Vec3d(k * -0.5D * f10, 0.025D * f10, 0.65D) : new Vec3d(k * -0.36D * f10, -0.045D * f10, 0.4D);
-                vec3d = vec3d.rotatePitch(-(player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * partialTicks) * 0.017453292F);
-                vec3d = vec3d.rotateYaw(-(player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * partialTicks) * 0.017453292F);
-                vec3d = vec3d.rotateYaw(f8 * 0.5F);
-                vec3d = vec3d.rotatePitch(-f8 * 0.7F);
-                d4 = player.prevPosX + (player.posX - player.prevPosX) * partialTicks + vec3d.x;
-                d5 = player.prevPosY + (player.posY - player.prevPosY) * partialTicks + vec3d.y;
-                d6 = player.prevPosZ + (player.posZ - player.prevPosZ) * partialTicks + vec3d.z;
+                double d8 = this.renderManager.options.fov;
+                d8 = d8 / 100.0D;
+                Vec3d vec3d = IndicatiaConfig.GENERAL.enableOldFishingRodRender.get() ? new Vec3d(i * -0.5D * d8, 0.025D * d8, 0.65D) : new Vec3d(i * -0.36D * d8, -0.045D * d8, 0.4D);
+                vec3d = vec3d.rotatePitch(-MathHelper.lerp(partialTicks, player.prevRotationPitch, player.rotationPitch) * ((float)Math.PI / 180F));
+                vec3d = vec3d.rotateYaw(-MathHelper.lerp(partialTicks, player.prevRotationYaw, player.rotationYaw) * ((float)Math.PI / 180F));
+                vec3d = vec3d.rotateYaw(f4 * 0.5F);
+                vec3d = vec3d.rotatePitch(-f4 * 0.7F);
+                d4 = MathHelper.lerp(partialTicks, player.prevPosX, player.posX) + vec3d.x;
+                d5 = MathHelper.lerp(partialTicks, player.prevPosY, player.posY) + vec3d.y;
+                d6 = MathHelper.lerp(partialTicks, player.prevPosZ, player.posZ) + vec3d.z;
                 d7 = player.getEyeHeight();
             }
             else
             {
                 double xz = player.isSneaking() ? 0.775D : IndicatiaConfig.GENERAL.enableOldFishingRodRender.get() ? 0.9D : 0.8D;
-                d4 = player.prevPosX + (player.posX - player.prevPosX) * partialTicks - d1 * d2 - d0 * xz;
+                d4 = MathHelper.lerp(partialTicks, player.prevPosX, player.posX) - d1 * d2 - d0 * xz;
                 d5 = player.prevPosY + player.getEyeHeight() + (player.posY - player.prevPosY) * partialTicks - (IndicatiaConfig.GENERAL.enableOldFishingRodRender.get() ? 0.4D : 0.45D);
-                d6 = player.prevPosZ + (player.posZ - player.prevPosZ) * partialTicks - d0 * d2 + d1 * xz;
-                d7 = player.isSneaking() ? IndicatiaConfig.GENERAL.enableOldFishingRodRender.get() ? -0.55D : -0.1875D : 0.0D;
+                d6 = MathHelper.lerp(partialTicks, player.prevPosZ, player.posZ) - d0 * d2 + d1 * xz;
+                d7 = player.shouldRenderSneaking() ? IndicatiaConfig.GENERAL.enableOldFishingRodRender.get() ? -0.55D : -0.1875D : 0.0D;
                 dz = IndicatiaConfig.GENERAL.enableOldFishingRodRender.get() && player.isSneaking() ? 0.01D : 0.0D;
             }
 
-            double d13 = entity.prevPosX + (entity.posX - entity.prevPosX) * partialTicks;
-            double d8 = entity.prevPosY + (entity.posY - entity.prevPosY) * partialTicks + 0.25D;
-            double d9 = entity.prevPosZ + (entity.posZ - entity.prevPosZ) * partialTicks;
+            double d13 = MathHelper.lerp(partialTicks, entity.prevPosX, entity.posX);
+            double d14 = MathHelper.lerp(partialTicks, entity.prevPosY, entity.posY) + 0.25D;
+            double d9 = MathHelper.lerp(partialTicks, entity.prevPosZ, entity.posZ);
             double d10 = (float)(d4 - d13) + dz;
-            double d11 = (float)(d5 - d8) + d7;
+            double d11 = (float)(d5 - d14) + d7;
             double d12 = (float)(d6 - d9);
             GlStateManager.disableTexture();
             GlStateManager.disableLighting();
-            vertexbuffer.begin(3, DefaultVertexFormats.POSITION_COLOR);
+            bufferbuilder.begin(3, DefaultVertexFormats.POSITION_COLOR);
 
-            for (int i1 = 0; i1 <= 16; ++i1)
+            for (int k = 0; k <= 16; ++k)
             {
-                float f11 = i1 / 16.0F;
-                vertexbuffer.pos(x + d10 * f11, y + d11 * (f11 * f11 + f11) * 0.5D + 0.25D, z + d12 * f11).color(0, 0, 0, 255).endVertex();
+                float f6 = k / 16.0F;
+                bufferbuilder.pos(x + d10 * f6, y + d11 * (f6 * f6 + f6) * 0.5D + 0.25D, z + d12 * f6).color(0, 0, 0, 255).endVertex();
             }
+
             tessellator.draw();
             GlStateManager.enableLighting();
             GlStateManager.enableTexture();
