9d2dfbe3e2e95bc3e1290515128525a2afb03ab2
SteveKunG <mccommander_minecraft@hotmail.com>
2021-03-27 19:09:04 +0700

    Port optifine fix

--- common/src/main/java/com/stevekung/indicatia/mixin/gui/components/MixinBossHealthOverlay.java
+++ common/src/main/java/com/stevekung/indicatia/mixin/gui/components/MixinBossHealthOverlay.java
@@ -1,10 +1,7 @@
 package com.stevekung.indicatia.mixin.gui.components;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.stevekung.indicatia.utils.PlatformConfig;
@@ -20,6 +17,12 @@ public class MixinBossHealthOverlay
         {
             info.cancel();
         }
+    }
+
+    @ModifyVariable(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;)V", at = @At(value = "INVOKE", target = "com/mojang/blaze3d/platform/Window.getGuiScaledHeight()I", shift = At.Shift.BEFORE), index = 3, ordinal = 1)
+    private int modifyIncrement(int defaultValue)
+    {
+        return PlatformConfig.getRenderBossHealthBar() ? defaultValue : 12;
     }
 
     @ModifyConstant(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;)V", constant = @Constant(intValue = 3))
