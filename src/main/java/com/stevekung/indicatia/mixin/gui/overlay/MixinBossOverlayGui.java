2a026852a01729e1d0d1f6954052314792412425
SteveKunG <mccommander_minecraft@hotmail.com>
2021-03-27 18:31:08 +0700

    Fix crash with optifine

--- src/main/java/com/stevekung/indicatia/mixin/gui/overlay/MixinBossOverlayGui.java
+++ src/main/java/com/stevekung/indicatia/mixin/gui/overlay/MixinBossOverlayGui.java
@@ -2,13 +2,13 @@ package com.stevekung.indicatia.mixin.gui.overlay;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.stevekung.indicatia.config.IndicatiaConfig;
 
 import net.minecraft.client.gui.overlay.BossOverlayGui;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 
 @Mixin(BossOverlayGui.class)
 public class MixinBossOverlayGui
@@ -22,10 +22,10 @@ public class MixinBossOverlayGui
         }
     }
 
-    @Redirect(method = "func_238484_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;)V", at = @At(value = "INVOKE", target = "net/minecraftforge/client/event/RenderGameOverlayEvent$BossInfo.getIncrement()I", remap = false))
-    private int modifyIncrement(RenderGameOverlayEvent.BossInfo event)
+    @ModifyVariable(method = "func_238484_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;)V", at = @At(value = "INVOKE", target = "net/minecraft/client/MainWindow.getScaledHeight()I", shift = Shift.BEFORE), index = 3, ordinal = 1)
+    private int modifyIncrement(int defaultValue)
     {
-        return IndicatiaConfig.GENERAL.enableBossHealthBarRender.get() ? event.getIncrement() : 12;
+        return IndicatiaConfig.GENERAL.enableBossHealthBarRender.get() ? defaultValue : 12;
     }
 
     @ModifyConstant(method = "func_238484_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;)V", constant = @Constant(intValue = 3))
