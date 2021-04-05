8c1bc4b44619c9048e33644aef9b260cf92ee593
SteveKunG <mccommander_minecraft@hotmail.com>
2021-04-05 18:30:53 +0700

    remove manual uncommented dev and production code from mixin

---
+++ forge/src/main/java/com/stevekung/indicatia/core/IndicatiaForgeMixinConfigPlugin.java
@@ -0,0 +1,46 @@
+package com.stevekung.indicatia.core;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class IndicatiaForgeMixinConfigPlugin implements IMixinConfigPlugin
+{
+    @Override
+    public void onLoad(String mixinPackage) {}
+
+    @Override
+    public String getRefMapperConfig()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName)
+    {
+        if (!FMLLoader.isProduction())
+        {
+            return !mixinClassName.equals("com.stevekung.indicatia.mixin.forge.renderer.entity.layers.MixinHumanoidArmorLayer");
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+    @Override
+    public List<String> getMixins()
+    {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+}
--- forge/src/main/java/com/stevekung/indicatia/mixin/forge/renderer/entity/layers/MixinHumanoidArmorLayerDev.java
+++ forge/src/main/java/com/stevekung/indicatia/mixin/forge/renderer/entity/layers/MixinHumanoidArmorLayerDev.java
@@ -18,10 +18,10 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 
 @Mixin(HumanoidArmorLayer.class)
-public class MixinHumanoidArmorLayer<T extends LivingEntity, M extends HumanoidModel<T>, A extends HumanoidModel<T>>
+public class MixinHumanoidArmorLayerDev<T extends LivingEntity, M extends HumanoidModel<T>, A extends HumanoidModel<T>>
 {
-    @Redirect(method = "renderArmorPiece", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/entity/layers/BipedArmorLayer.renderModel(Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;IZLnet/minecraft/client/renderer/entity/model/BipedModel;FFFLnet/minecraft/util/ResourceLocation;)V", remap = false), require = 3, allow = 3)
-    private void renderArmorProd(HumanoidArmorLayer<T, M, A> armorLayer, PoseStack _poseStack, MultiBufferSource _bufferIn, int _packedLightIn, boolean glintIn, A modelIn, float red, float green, float blue, ResourceLocation armorResource, PoseStack poseStack, MultiBufferSource buffer, T entityLivingBaseIn, EquipmentSlot slot, int packedLight, A model)
+    @Redirect(method = "renderArmorPiece", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/entity/layers/HumanoidArmorLayer.renderModel(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IZLnet/minecraft/client/model/HumanoidModel;FFFLnet/minecraft/resources/ResourceLocation;)V", remap = false), require = 3, allow = 3)
+    private void renderArmorDev(HumanoidArmorLayer<T, M, A> armorLayer, PoseStack _poseStack, MultiBufferSource _bufferIn, int _packedLightIn, boolean glintIn, A modelIn, float red, float green, float blue, ResourceLocation armorResource, PoseStack poseStack, MultiBufferSource buffer, T entityLivingBaseIn, EquipmentSlot slot, int packedLight, A model)
     {
         this.renderArmorModified(_poseStack, _bufferIn, entityLivingBaseIn, _packedLightIn, glintIn, modelIn, red, green, blue, armorResource);
     }
