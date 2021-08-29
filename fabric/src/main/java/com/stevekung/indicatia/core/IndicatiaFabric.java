056fad8ee41b5e1c1786b45654cd0a68d63256b9
SteveKunG <mccommander_minecraft@hotmail.com>
2021-08-30 03:32:27 +0700

    Add antisteal

--- fabric/src/main/java/com/stevekung/indicatia/core/IndicatiaFabric.java
+++ fabric/src/main/java/com/stevekung/indicatia/core/IndicatiaFabric.java
@@ -10,6 +10,7 @@ import com.stevekung.indicatia.event.IndicatiaEventHandler;
 import com.stevekung.indicatia.gui.exconfig.screens.ExtendedConfigScreen;
 import com.stevekung.indicatia.handler.KeyBindingHandler;
 import com.stevekung.indicatia.utils.hud.HUDHelper;
+import com.stevekung.stevekungslib.utils.CommonUtils;
 import com.stevekung.stevekungslib.utils.client.ClientRegistryUtils;
 import dev.architectury.event.events.client.ClientPlayerEvent;
 import me.shedaniel.autoconfig.AutoConfig;
@@ -20,6 +21,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 
 public class IndicatiaFabric implements ClientModInitializer
 {
@@ -34,6 +36,8 @@ public class IndicatiaFabric implements ClientModInitializer
     public void onInitializeClient()
     {
         Indicatia.init();
+        CommonUtils.initAntisteal("indicatia", IndicatiaFabric.class, () -> Minecraft.getInstance().close());
+
         ClientRegistryUtils.registerKeyBinding(Indicatia.keyBindAltChat);
 
         AutoConfig.register(IndicatiaConfig.class, GsonConfigSerializer::new);
