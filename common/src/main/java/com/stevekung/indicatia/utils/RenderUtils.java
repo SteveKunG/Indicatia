775c8c859e7d449928c38801871fd261cdb8c177
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2023-02-11 06:02:58 +0700

    Added support for infinite duration

--- common/src/main/java/com/stevekung/indicatia/utils/RenderUtils.java
+++ common/src/main/java/com/stevekung/indicatia/utils/RenderUtils.java
@@ -3,7 +3,6 @@ package com.stevekung.indicatia.utils;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.util.Mth;
@@ -14,10 +13,14 @@ public class RenderUtils
 {
     public static void renderPotionDurationOnTopRight(Minecraft minecraft, PoseStack poseStack, MobEffectInstance mobEffectInstance, int x, int y, float alpha)
     {
+        var isInfinite = mobEffectInstance.isInfiniteDuration();
         var ticks = Mth.floor((float) mobEffectInstance.getDuration());
-        var text = Component.literal(StringUtil.formatTickDuration(ticks)).withStyle(Style.EMPTY.withFont(Minecraft.UNIFORM_FONT));
+        var component = isInfinite ? Component.translatable("effect.duration.infinite") : Component.literal(StringUtil.formatTickDuration(ticks));
+        var text = component.withStyle(Style.EMPTY.withFont(isInfinite ? null : Minecraft.UNIFORM_FONT));
         var color = 0xFFFFFF | Mth.floor(alpha * 255.0F) << 24 & 0xFF000000;
-        GuiComponent.drawCenteredString(poseStack, minecraft.font, text, x + 12, y + 15, color);
+        var formattedCharSequence = text.getVisualOrderText();
+        var xf = (float)(x - minecraft.font.width(formattedCharSequence) / 2);
+        minecraft.font.drawShadow(poseStack, formattedCharSequence, xf + 12f + (isInfinite ? 0.5f : 0f), y + 15, color);
         RenderSystem.enableBlend();
     }
 }
