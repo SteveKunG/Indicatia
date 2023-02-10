4c6c14ffc1979b4c4dd72a95cee445ce7b5c8521
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2023-02-11 05:54:48 +0700

    Fix Bad local variable type

--- common/src/main/java/com/stevekung/indicatia/mixin/renderer/entity/layers/MixinCustomHeadLayer.java
+++ common/src/main/java/com/stevekung/indicatia/mixin/renderer/entity/layers/MixinCustomHeadLayer.java
@@ -1,10 +1,11 @@
 package com.stevekung.indicatia.mixin.renderer.entity.layers;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import com.mojang.authlib.GameProfile;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.stevekung.indicatia.utils.EnchantedSkullTileEntityRenderer;
@@ -13,16 +14,50 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.SkullBlock;
 
 @Mixin(CustomHeadLayer.class)
 public class MixinCustomHeadLayer<T extends LivingEntity>
 {
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/blockentity/SkullBlockRenderer.renderSkull(Lnet/minecraft/core/Direction;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/SkullModelBase;Lnet/minecraft/client/renderer/RenderType;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void indicatia$renderEnchantedSkull(PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo info, ItemStack itemStack, Item item, boolean bl, float f, GameProfile gameProfile, SkullBlock.Type type, SkullModelBase skullModelBase, RenderType renderType)
+    @Unique
+    private GameProfile gameProfile;
+
+    @Unique
+    private SkullModelBase skullModelBase;
+
+    @Unique
+    private RenderType renderType;
+
+    @Unique
+    private ItemStack itemStack;
+
+    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/blockentity/SkullBlockRenderer.renderSkull(Lnet/minecraft/core/Direction;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/SkullModelBase;Lnet/minecraft/client/renderer/RenderType;)V"), index = 16)
+    private GameProfile indicatia$getGameProfile(GameProfile gameProfile)
     {
-        EnchantedSkullTileEntityRenderer.render(gameProfile, 180.0F, limbSwing, poseStack, multiBufferSource, packedLight, skullModelBase, renderType, itemStack.hasFoil());
+        return this.gameProfile = gameProfile;
+    }
+
+    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/blockentity/SkullBlockRenderer.renderSkull(Lnet/minecraft/core/Direction;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/SkullModelBase;Lnet/minecraft/client/renderer/RenderType;)V"), index = 18)
+    private SkullModelBase indicatia$getSkullModelBase(SkullModelBase skullModelBase)
+    {
+        return this.skullModelBase = skullModelBase;
+    }
+
+    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/blockentity/SkullBlockRenderer.renderSkull(Lnet/minecraft/core/Direction;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/SkullModelBase;Lnet/minecraft/client/renderer/RenderType;)V"), index = 19)
+    private RenderType indicatia$getRenderType(RenderType renderType)
+    {
+        return this.renderType = renderType;
+    }
+
+    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/blockentity/SkullBlockRenderer.renderSkull(Lnet/minecraft/core/Direction;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/SkullModelBase;Lnet/minecraft/client/renderer/RenderType;)V"), index = 11)
+    private ItemStack indicatia$getItemStack(ItemStack itemStack)
+    {
+        return this.itemStack = itemStack;
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/blockentity/SkullBlockRenderer.renderSkull(Lnet/minecraft/core/Direction;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/SkullModelBase;Lnet/minecraft/client/renderer/RenderType;)V"))
+    private void indicatia$renderEnchantedSkull(PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo info)
+    {
+        EnchantedSkullTileEntityRenderer.render(this.gameProfile, 180.0F, limbSwing, poseStack, multiBufferSource, packedLight, this.skullModelBase, this.renderType, this.itemStack.hasFoil());
     }
 }
