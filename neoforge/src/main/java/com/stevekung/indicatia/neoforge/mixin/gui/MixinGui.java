b9aa2effd9e6902fd3bdbdfd2c57a6f49c6bbcc9
SteveKunG <mccommander_minecraft@hotmail.com>
2024-06-14 11:49:41 +0700

    Update to 1.21

--- neoforge/src/main/java/com/stevekung/indicatia/neoforge/mixin/gui/MixinGui.java
+++ neoforge/src/main/java/com/stevekung/indicatia/neoforge/mixin/gui/MixinGui.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.stevekung.indicatia.Indicatia;
 import com.stevekung.indicatia.utils.RenderUtils;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
@@ -25,7 +26,7 @@ public class MixinGui
     Minecraft minecraft;
 
     @Inject(method = "renderEffects", at = @At(value = "INVOKE", target = "java/util/List.add(Ljava/lang/Object;)Z", shift = At.Shift.AFTER, remap = false), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void indicatia$addPotionTime(GuiGraphics guiGraphics, float partialTick, CallbackInfo info, @Local List<Runnable> list, @Local MobEffectInstance mobEffectInstance, @Local(index = 12, ordinal = 2) int x, @Local(index = 13, ordinal = 3) int y, @Local(index = 14, ordinal = 1) float alpha)
+    private void indicatia$addPotionTime(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo info, @Local List<Runnable> list, @Local MobEffectInstance mobEffectInstance, @Local(index = 12, ordinal = 2) int x, @Local(index = 13, ordinal = 3) int y, @Local(index = 14, ordinal = 1) float alpha)
     {
         if (Indicatia.CONFIG.displayPotionDurationOnTopRightPotionHUD)
         {
