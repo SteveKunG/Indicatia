d49d2a5a7fa03d1ac0c7675a9d6dd845176a9cf5
SteveKunG <mccommander_minecraft@hotmail.com>
2025-07-20 22:01:59 +0700

    fix: update MixinGui for neoforge

--- neoforge/src/main/java/com/stevekung/indicatia/neoforge/mixin/gui/MixinGui.java
+++ neoforge/src/main/java/com/stevekung/indicatia/neoforge/mixin/gui/MixinGui.java
@@ -1,17 +1,16 @@
 package com.stevekung.indicatia.neoforge.mixin.gui;
 
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
 import com.llamalad7.mixinextras.sugar.Local;
 import com.stevekung.indicatia.Indicatia;
 import com.stevekung.indicatia.utils.RenderUtils;
+
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -25,12 +24,15 @@ public class MixinGui
     @Final
     Minecraft minecraft;
 
-    @Inject(method = "renderEffects", at = @At(value = "INVOKE", target = "java/util/List.add(Ljava/lang/Object;)Z", shift = At.Shift.AFTER, remap = false), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void indicatia$addPotionTime(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo info, @Local List<Runnable> list, @Local MobEffectInstance mobEffectInstance, @Local(index = 12, ordinal = 2) int x, @Local(index = 13, ordinal = 3) int y, @Local(index = 14, ordinal = 1) float alpha)
+    @Inject(method = "renderEffects", at = @At(
+            value = "INVOKE",
+            target = "net/minecraft/client/gui/GuiGraphics.blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIIII)V",
+            shift = At.Shift.AFTER))
+    private void indicatia$addPotionTime(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo info, @Local MobEffectInstance mobEffectInstance, @Local(index = 10, ordinal = 2) int x, @Local(index = 11, ordinal = 3) int y, @Local(index = 12) float alpha)
     {
         if (Indicatia.CONFIG.displayPotionDurationOnTopRightPotionHUD)
         {
-            list.add(() -> RenderUtils.renderPotionDurationOnTopRight(Gui.class.cast(this).getFont(), guiGraphics, mobEffectInstance, x, y, alpha, this.minecraft.level.tickRateManager().tickrate()));
+            RenderUtils.renderPotionDurationOnTopRight(this.minecraft.font, guiGraphics, mobEffectInstance, x, y, alpha, this.minecraft.level.tickRateManager().tickrate());
         }
     }
 }
