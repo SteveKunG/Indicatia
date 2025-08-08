8507d3c62a3a2559ae0b3bbc712fa4ccaf5c7226
SteveKunG <mccommander_minecraft@hotmail.com>
2025-08-08 11:38:31 +0700

    chore: update to 25w32a

--- common/src/main/java/com/stevekung/indicatia/utils/RenderUtils.java
+++ common/src/main/java/com/stevekung/indicatia/utils/RenderUtils.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FontDescription;
 import net.minecraft.network.chat.Style;
 import net.minecraft.util.Mth;
 import net.minecraft.util.StringUtil;
@@ -11,12 +12,14 @@ import net.minecraft.world.effect.MobEffectInstance;
 
 public class RenderUtils
 {
+    private static final FontDescription UNIFORM = new FontDescription.Resource(Minecraft.UNIFORM_FONT);
+
     public static void renderPotionDurationOnTopRight(Font font, GuiGraphics guiGraphics, MobEffectInstance mobEffectInstance, int x, int y, float alpha, float ticksPerSecond)
     {
         var isInfinite = mobEffectInstance.isInfiniteDuration();
         var ticks = Mth.floor((float) mobEffectInstance.getDuration());
         var component = isInfinite ? Component.translatable("effect.duration.infinite") : Component.literal(StringUtil.formatTickDuration(ticks, ticksPerSecond));
-        var text = component.withStyle(Style.EMPTY.withFont(isInfinite ? null : Minecraft.UNIFORM_FONT));
+        var text = component.withStyle(Style.EMPTY.withFont(isInfinite ? null : UNIFORM));
         var color = 0xFFFFFF | Mth.floor(alpha * 255.0F) << 24 & 0xFF000000;
         guiGraphics.drawCenteredString(font, text, x + 12, y + 15, color);
     }
