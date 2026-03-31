28ff23cc351a93c9ff90779b45a954fac32f3d96
SteveKunG <mccommander_minecraft@hotmail.com>
2026-03-31 10:24:21 +0700

    ci: update deps

--- fabric/src/main/java/com/stevekung/indicatia/fabric/modmenu/ModMenuIntegrationIN.java
+++ fabric/src/main/java/com/stevekung/indicatia/fabric/modmenu/ModMenuIntegrationIN.java
@@ -3,13 +3,13 @@ package com.stevekung.indicatia.fabric.modmenu;
 import com.stevekung.indicatia.config.IndicatiaConfig;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuIntegrationIN implements ModMenuApi
 {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory()
     {
-        return parent -> AutoConfig.getConfigScreen(IndicatiaConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(IndicatiaConfig.class, parent).get();
     }
 }
